9b5d4ef9bb3dc6fc32d46aa49566cfd9744943ab
silviasalete <silvia.samtech@gmail.com>
2021-03-11 19:38:38 -0300

    :lock: Change from statement to preparedStatemet class Database

--- src/com/okr/model/dao/DataBase.java
+++ src/com/okr/model/dao/DataBase.java
@@ -1,9 +1,9 @@
 package com.okr.model.dao;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -25,9 +25,14 @@ public class DataBase {
 		
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute("INSERT INTO user (name, email, password, company_id) VALUES ('"+user.getName()+"','"+user.getEmail()+"','"+user.getPassword()+"',0);",Statement.RETURN_GENERATED_KEYS);
-			ResultSet 	resultSet = statement.getGeneratedKeys();
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO user (name, email, password, company_id) VALUES (?,?,?,?);",PreparedStatement.RETURN_GENERATED_KEYS);
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getEmail());
+			preparedStatement.setString(3, user.getPassword());
+			preparedStatement.setInt(4, 0);
+			preparedStatement.execute();
+			
+			ResultSet 	resultSet = preparedStatement.getGeneratedKeys();
 			
 			while(resultSet.next()) {
 				generate_key =  resultSet.getInt(1); 
@@ -54,10 +59,11 @@ public class DataBase {
 		Connection connection = factory.getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			String 			sql = "SELECT * FROM user WHERE email = '"+email+"' AND password = '"+password+"';";	
-			statement.execute(sql);
-					  resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE email = ? AND password = ?");
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			preparedStatement.execute();
+			resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				
@@ -89,15 +95,15 @@ public class DataBase {
 
 	public List<Objective> getListObjectiveByUser(User user) {
 		
-		List<Objective> listObjectiveReturn = new ArrayList<>();		
-		String 							sql = "SELECT * FROM objective WHERE createdById = "+user.getId();		
+		List<Objective> listObjectiveReturn = new ArrayList<>();
 		Connection 				 connection = new ConnectionFactory().getConnection();
 		
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM objective WHERE createdById = ?");
+			preparedStatement.setInt(1, user.getId());
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 
@@ -113,16 +119,15 @@ public class DataBase {
 	}
 
 	public User getUserById(int id) {
-		
-		String sql = "SELECT * FROM user WHERE id = "+id;
 
 		User user = null;
 		Connection connection = new ConnectionFactory().getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE id = ?");
+			preparedStatement.setInt(1, id);
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				
@@ -140,16 +145,25 @@ public class DataBase {
 
 	public boolean addObjective(Objective objective) {
 		
-		Connection connection = factory.getConnection();
+		Connection    connection = factory.getConnection();
 		boolean createdObjective = false;
 		try {
 			Date date = new Date();
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			String data = simpleDateFormat.format(date);
 
-			Statement statement = connection.createStatement();
-			statement.execute("INSERT INTO objective (context, image, periodId, privacyId, startObjective, endObjective, createdIn, updatedIn,createdById) VALUES ('"+objective.getDescription()+"','image',1,'PRIVATE','2021-03-01','2022-03-01','"+data+"','"+data+"',"+objective.getUser().getId()+");",Statement.RETURN_GENERATED_KEYS);
-			ResultSet 	resultSet = statement.getGeneratedKeys();
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO objective (context, image, periodId, privacyId, startObjective, endObjective, createdIn, updatedIn,createdById) VALUES (?,?,?,?,?,?,?,?,?);",PreparedStatement.RETURN_GENERATED_KEYS);
+			preparedStatement.setString(1, objective.getDescription());
+			preparedStatement.setString(2, "image");
+			preparedStatement.setInt(3, 1);
+			preparedStatement.setString(4, "PRIVATE");
+			preparedStatement.setString(5, "2021-03-01");
+			preparedStatement.setString(6, "2022-03-01");
+			preparedStatement.setString(7, data);
+			preparedStatement.setString(8, data);
+			preparedStatement.setInt(9, objective.getUser().getId());
+			preparedStatement.execute();
+			ResultSet 	resultSet = preparedStatement.getGeneratedKeys();
 			
 			while(resultSet.next()) {
 				Integer id =  resultSet.getInt(1);
@@ -171,14 +185,14 @@ public class DataBase {
 
 	public Objective getObjectiveById(int id) {
 		
-		Objective  objective = null;		
-		String 			 sql = "SELECT * FROM objective WHERE id = "+id;
+		Objective  objective = null;	
 		Connection connection = new ConnectionFactory().getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM objective WHERE id = ?");
+			preparedStatement.setInt(1, id);
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				User user = getUserById(resultSet.getInt("createdById"));
@@ -195,15 +209,16 @@ public class DataBase {
 
 	public boolean updateObjective(Objective objective) {
 		
-		Connection 	  connection = new ConnectionFactory().getConnection();		
-		String 				 sql = "UPDATE objective SET context = '"+objective.getDescription()+"' WHERE id = "+objective.getId();
+		Connection 	  connection = new ConnectionFactory().getConnection();
 		Integer 	updatedLines = 0;
 		
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			updatedLines = statement.getUpdateCount();
+			PreparedStatement preparedStatement = connection.prepareStatement("UPDATE objective SET context = ? WHERE id = ?");
+			preparedStatement.setString(1, objective.getDescription());
+			preparedStatement.setInt(2, objective.getId());
+			preparedStatement.execute();
+			updatedLines = preparedStatement.getUpdateCount();
 			System.out.println("Linhas alteradas: "+updatedLines);
 			
 		} catch (SQLException e) {
@@ -217,15 +232,16 @@ public class DataBase {
 
 	public boolean removeObjective(int idObjective, int idUser) { 
 		
-		Connection 	  connection = new ConnectionFactory().getConnection();		
-		String 				 sql = "DELETE FROM objective WHERE ID = "+idObjective+" AND createdById = "+idUser;
+		Connection 	  connection = new ConnectionFactory().getConnection();
 		Integer 	updatedLines = 0;
 		
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			updatedLines = statement.getUpdateCount();
+			PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM objective WHERE ID = ? AND createdById = ?");
+			preparedStatement.setInt(1, idObjective);
+			preparedStatement.setInt(2, idUser);
+			preparedStatement.execute();
+			updatedLines = preparedStatement.getUpdateCount();
 
 			
 		} catch (SQLException e) {
@@ -246,10 +262,25 @@ public class DataBase {
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			String data = simpleDateFormat.format(date);
 
-			Statement statement = connection.createStatement();
-			statement.execute("INSERT INTO keyresult (objective,verb,counter,typeCounter,noun,complement,period,controlPeriod,percentageReached,startKeyResult,endKeyResult,createdIn,updatedIn,createdById) VALUES "
-					+ "("+keyResult.getIdObjective()+",'fazer',1,'numero','mensagens','"+keyResult.getDescription()+"',30,30,0,'"+data+"','"+data+"','"+data+"','"+data+"',"+keyResult.getUser().getId()+");",Statement.RETURN_GENERATED_KEYS);
-			ResultSet 	resultSet = statement.getGeneratedKeys();
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO keyresult (objective,verb,counter,typeCounter,noun,complement,period,controlPeriod,percentageReached,startKeyResult,endKeyResult,createdIn,updatedIn,createdById) VALUES "
+					+ "(?,?,?,?,?,?,?,?,?,?,?,?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
+			preparedStatement.setInt(1, keyResult.getIdObjective());
+			preparedStatement.setString(2, "fazer");
+			preparedStatement.setInt(3, 1);
+			preparedStatement.setString(4, "numero");
+			preparedStatement.setString(5, "mensagens");
+			preparedStatement.setString(6, keyResult.getDescription());
+			preparedStatement.setInt(7, 30);
+			preparedStatement.setInt(8, 30);
+			preparedStatement.setInt(9, 0);
+			preparedStatement.setString(10, data);
+			preparedStatement.setString(11, data);
+			preparedStatement.setString(12, data);
+			preparedStatement.setString(13, data);
+			preparedStatement.setInt(14, keyResult.getUser().getId());			
+			preparedStatement.execute();
+			
+			ResultSet 	resultSet = preparedStatement.getGeneratedKeys();
 			
 			while(resultSet.next()) {
 				Integer id =  resultSet.getInt(1);
@@ -271,14 +302,14 @@ public class DataBase {
 	
 	public KeyResult getKeyResultById(int id) {
 		
-		KeyResult  keyResult = new KeyResult();		
-		String 			 sql = "SELECT * FROM keyresult WHERE id = "+id;
+		KeyResult  keyResult = new KeyResult();
 		Connection connection = new ConnectionFactory().getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM keyresult WHERE id = ?");
+			preparedStatement.setInt(1, id);
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				User user = getUserById(resultSet.getInt("createdById"));
@@ -296,14 +327,17 @@ public class DataBase {
 
 	public boolean updateKeyResult(KeyResult keyResult) {
 		
-		Connection 	  connection = new ConnectionFactory().getConnection();		
-		String 				 sql = "UPDATE keyresult SET complement = '"+keyResult.getDescription()+"' WHERE id = "+keyResult.getId()+" and objective = "+keyResult.getIdObjective()+" and createdById = "+keyResult.getUser().getId();
+		Connection 	  connection = new ConnectionFactory().getConnection();
 		Integer 	updatedLines = 0;
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			updatedLines = statement.getUpdateCount();
+			PreparedStatement preparedStatement = connection.prepareStatement("UPDATE keyresult SET complement = ? WHERE id = ? and objective = ? and createdById = ?");
+			preparedStatement.setString(1, keyResult.getDescription());
+			preparedStatement.setInt(2, keyResult.getId());
+			preparedStatement.setInt(3, keyResult.getIdObjective());
+			preparedStatement.setInt(4, keyResult.getUser().getId());
+			preparedStatement.execute();
+			updatedLines = preparedStatement.getUpdateCount();
 			
 		} catch (SQLException e) {
 
@@ -315,15 +349,17 @@ public class DataBase {
 
 	public boolean removeKeyResult(int idUser, int idObjective, int idKeuResult) {
 		
-		Connection 	  connection = new ConnectionFactory().getConnection();		
-		String 				 sql = "DELETE FROM keyresult WHERE ID = "+idKeuResult+" AND createdById = "+idUser+" AND objective = "+idObjective;
+		Connection 	  connection = new ConnectionFactory().getConnection();
 		Integer 	updatedLines = 0;
 		
 		try {
 			
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			updatedLines = statement.getUpdateCount();
+			PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM keyresult WHERE ID = ? AND createdById = ? AND objective = ?");
+			preparedStatement.setInt(1, idKeuResult);
+			preparedStatement.setInt(2, idUser);
+			preparedStatement.setInt(3, idObjective);
+			preparedStatement.execute();
+			updatedLines = preparedStatement.getUpdateCount();
 
 			
 		} catch (SQLException e) {
@@ -336,14 +372,15 @@ public class DataBase {
 
 	public List<KeyResult> getListKeyResultByObjectiveId(int idObjective, int idUser) {
 		
-		List<KeyResult> listKeyResult = new ArrayList<KeyResult>();		
-		String 			 		  sql = "SELECT * FROM keyresult WHERE objective = "+idObjective+" and createdById = "+idUser;
+		List<KeyResult> listKeyResult = new ArrayList<KeyResult>();		 
 		Connection 		   connection = new ConnectionFactory().getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM keyresult WHERE objective = ? and createdById = ?");
+			preparedStatement.setInt(1, idObjective);
+			preparedStatement.setInt(2, idUser);
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				User user = getUserById(resultSet.getInt("createdById"));
@@ -360,14 +397,16 @@ public class DataBase {
 
 	public KeyResult getKeyResultByIdUserObjectiveKeyResult(int idUser, int idObjective, int idKeyResult) {
 		
-		KeyResult  keyResult = new KeyResult();		
-		String 			 sql = "SELECT * FROM keyresult WHERE id = "+idKeyResult+"and objective = "+idObjective+" and createdById = "+idUser;
+		KeyResult  keyResult = new KeyResult();
 		Connection connection = new ConnectionFactory().getConnection();
 		
 		try {
-			Statement statement = connection.createStatement();
-			statement.execute(sql);
-			ResultSet resultSet = statement.getResultSet();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM keyresult WHERE id = ? and objective = ? and createdById = ?");
+			preparedStatement.setInt(1, idKeyResult);
+			preparedStatement.setInt(2, idObjective);
+			preparedStatement.setInt(3, idUser);
+			preparedStatement.execute();
+			ResultSet resultSet = preparedStatement.getResultSet();
 			
 			while(resultSet.next()) {
 				User user = getUserById(resultSet.getInt("createdById"));
